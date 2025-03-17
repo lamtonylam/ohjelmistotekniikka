2f445876236d1a4798e6286410d8ef3dcd6198c3
Tony Lam <tony.lam@iki.fi>
2025-03-17 14:31:02 +0200

    t7 : vk2

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -6,6 +6,9 @@ class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(1000)
 
+    def test_kortti_luodaan_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
 
