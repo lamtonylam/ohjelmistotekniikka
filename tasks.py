e6e4663b4372ca87ff7b9cc5f0565f4d38e6ddc0
Tony Lam <tony.lam@iki.fi>
2025-03-26 11:37:58 +0200

    Feat: add coverage support and add invoke commands

--- tasks.py
+++ tasks.py
@@ -4,3 +4,23 @@ from invoke import task
 @task
 def build(ctx):
     ctx.run("python3 src/db/initialize_database.py", pty=True)
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
