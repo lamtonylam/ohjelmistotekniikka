d03e6336bee12b05b6ef052d112e51ca62675b1a
Tony Lam <tony.lam@iki.fi>
2025-04-23 16:48:20 +0300

    feat: use test database file when running pytest

--- src/db/database_connection.py
+++ src/db/database_connection.py
@@ -1,9 +1,16 @@
 import os
 import sqlite3
+import sys
 
 dirname = os.path.dirname(__file__)
 
-DATABASE_FILE_PATH = os.path.join(dirname, "..", "..", "database.db")
+
+# if the program is run in pytest mode, set the database file path to the test database
+# otherwise, set it to the production database
+if "pytest" in sys.modules:
+    DATABASE_FILE_PATH = os.path.join(dirname, "..", "..", "test_database.db")
+else:
+    DATABASE_FILE_PATH = os.path.join(dirname, "..", "..", "database.db")
 
 connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory = sqlite3.Row
