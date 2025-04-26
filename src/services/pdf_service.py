c12adeee63f1a567a82f6f24195786b263e7f2ca
Tony Lam <tony.lam@iki.fi>
2025-04-26 16:05:55 +0300

    Feat: pdf export with users and matches

--- src/services/pdf_service.py
+++ src/services/pdf_service.py
@@ -1,8 +1,7 @@
 from reportlab.pdfgen import canvas
 
-from repositories.match_repository import match_repository as default_match_repository
-from repositories.user_repository import user_repository as default_user_repository
 from .elo_service import EloService
+from .match_service import MatchService
 
 
 class PdfService:
@@ -10,20 +9,19 @@ class PdfService:
 
     def __init__(
         self,
-        match_repository=default_match_repository,
-        user_repostory=default_user_repository,
     ):
-        self.match_repository = match_repository
-        self._user_repository = user_repostory
         self.elo_service = EloService()
+        self.match_service = MatchService()
 
     def generate_pdf(self):
         """
         Generates a PDF report of the matches played.
         """
-        c = canvas.Canvas(
-            "PDF-export.pdf", pagesize=(595.27, 841.89)
-        )  # A4 pagesize
+
+        users = self.elo_service.get_all_users()
+        matches = self.match_service.get_all_matches()
+
+        c = canvas.Canvas("PDF-export.pdf", pagesize=(595.27, 841.89))  # A4 pagesize
         # draw a string at x=100, y=800 points
         # point ~ standard desktop publishing (72 DPI)
         # coordinate system:
@@ -33,7 +31,72 @@ class PdfService:
         #   |
         #   |
         #   0-------x
-        c.drawString(50, 780, "FIFA Eloranking")
+
+        # set title
+        c.setFont("Helvetica-Bold", 18)
+        c.drawString(50, 800, "FIFA Eloranking PDF Report")
+
+        # set subtitle
+        c.setFont("Helvetica-Bold", 14)
+        c.drawString(50, 770, "Users")
+
+        # set font for user list
+        c.setFont("Helvetica", 12)
+        # start y position for the user list from 740
+        y = 740
+        line_height = 20
+
+        # check if there are any users to display
+        if not users:
+            c.drawString(50, y, "No users found.")
+        else:
+            # iterate through users and print their name and Elo rating
+            for user in users:
+                if len(user.name) > 40:
+                    # if name too long, show only first 10 characters and add "..."
+                    user.name = user.name[:20] + "..."
+                c.drawString(50, y, f"Name: {user.name}")
+                c.drawString(400, y, f"Elo ranking: {user.elo_rating}")
+                y -= line_height
+            # if the y position is too low, start a new page
+            if y < 50:
+                c.showPage()
+                y = 800
+                c.setFont("Helvetica", 12)
+
+        # set subtitle
+        c.setFont("Helvetica-Bold", 14)
+        c.drawString(50, y - 50, "Matches")
+        y -= 70
+
+        # set font for match list
+        c.setFont("Helvetica", 12)
+
+        if not matches:
+            c.drawString(50, y, "No matches found.")
+        else:
+            for match in matches:
+                winner_name = self.elo_service.find_user_by_id(match.winner).name
+                loser_name = self.elo_service.find_user_by_id(match.loser).name
+
+            # if name too long, show only first 10 characters and add "..."
+            max_name_length = 15
+            if len(winner_name) > max_name_length:
+                winner_name = winner_name[:max_name_length] + "..."
+            if len(loser_name) > max_name_length:
+                loser_name = loser_name[:max_name_length] + "..."
+
+            c.drawString(50, y, f"Winner: {winner_name}")
+            c.drawString(200, y, f"Loser: {loser_name}")
+            c.drawString(400, y, f"Date: {match.date}")
+            y -= line_height
+
+            # if the y position is too low, start a new page
+            if y < 50:
+                c.showPage()
+                y = 800
+                c.setFont("Helvetica", 12)
+
         # finish page
         c.showPage()
         # construct and save file to .pdf
