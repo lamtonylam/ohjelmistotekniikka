d40f0544e94b3925a6b7c80711e44a0b81831509
Tony Lam <tony.lam@iki.fi>
2025-05-02 13:03:38 +0300

    update: add more tests for user functions

--- src/tests/users_test.py
+++ src/tests/users_test.py
@@ -4,6 +4,7 @@ from services.elo_service import EloService
 
 from build import build
 
+
 class TestUserCreation(unittest.TestCase):
     def setUp(self):
         build()
@@ -12,9 +13,25 @@ class TestUserCreation(unittest.TestCase):
     def test_getting_all_users(self):
         self.user_service.create_user("tomppa1")
         self.user_service.create_user("tomppa2")
-        
+
         all_users = self.user_service.get_all_users()
 
         self.assertEqual(len(all_users), 2)
         self.assertEqual(all_users[0].name, "tomppa1")
         self.assertEqual(all_users[1].name, "tomppa2")
+
+    def test_adding_user(self):
+        self.user_service.create_user("tomppa")
+        all_users = self.user_service.get_all_users()
+
+        self.assertEqual(len(all_users), 1)
+        self.assertEqual(all_users[0].name, "tomppa")
+
+    def test_adding_user_with_same_name_doesnt_add_two_users(self):
+        self.user_service.create_user("tomppa")
+        with self.assertRaises(Exception):
+            self.user_service.create_user("tomppa")
+
+        all_users = self.user_service.get_all_users()
+        self.assertEqual(len(all_users), 1)
+        self.assertEqual(all_users[0].name, "tomppa")
